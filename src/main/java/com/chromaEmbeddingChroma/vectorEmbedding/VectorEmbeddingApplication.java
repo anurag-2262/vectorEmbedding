e1b81d9b54a9ef3eba803c3363d3de74e4aec4a5
anurag-2262 <anuraglakhara125@gmail.com>
2024-09-06 15:11:23 +0530

    Initial commit

---
+++ src/main/java/com/chromaEmbeddingChroma/vectorEmbedding/VectorEmbeddingApplication.java
@@ -0,0 +1,13 @@
+package com.chromaEmbeddingChroma.vectorEmbedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorEmbeddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorEmbeddingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chromaEmbeddingChroma/vectorEmbedding/VectorEmbeddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chromaEmbeddingChroma.vectorEmbedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorEmbeddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
